613fc2b4606f86fb9223da0676b7572c94c82951
White Rabbit <white3001@gmail.com>
2024-05-24 18:26:37 +0200

    added toggling cards

---
+++ Memory/TestConnectOpenAI/TestConnectOpenAI/TestConnectOpenAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestConnectOpenAIApp.swift
+//  TestConnectOpenAI
+//
+//  Created by Flavien Berthold on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestConnectOpenAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
